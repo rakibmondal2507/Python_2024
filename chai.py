176bdf14418cc8bb16a1b883ca799313cfd611b4
Rakib Mondal <134124152+rakibmondal2507@users.noreply.github.com>
2024-02-09 20:30:47 +0530

    Add files via upload

---
+++ chai.py
@@ -0,0 +1,5 @@
+from hello_chai import chai
+
+chai("Ginger Tea")
+
+chai
---
+++ hello_chai.py
@@ -0,0 +1,10 @@
+print("Hello , Chai")
+
+def chai(n):
+    print(n)
+    
+chai("lemon tea")
+
+chai_one = "lemon tea"
+chai_two = "Ginger tea"
+chai_three = "Masala chai"
